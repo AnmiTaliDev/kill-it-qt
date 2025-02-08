ec5d0fd1d8028a9003ffbeba91d107981e7fbc16
ruze42 <malledeferlive@gmail.com>
2025-02-09 00:22:25 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+class Program
+{
+    public static void Main()
+    {
+        Application.Init();
+        var App = new KillIt();
+        App.Resizable = true;
+        App.DeleteEvent += delegate
+        {
+            Console.WriteLine("App Closed");
+            Application.Quit();
+        };        
+        Application.Run();
+    }
+}
